91d771de31393b5674c3609adfc8b36d531e2fae
GiaBao <tranduonggiabao0501@gmail.com>
2026-03-17 23:36:37 +0700

    final day 5

--- BE_WPF/B5_MVVM/B5_MVVM/Bai4/Views/StudentView.xaml.cs
+++ BE_WPF/B5_MVVM/B5_MVVM/Bai4/Views/StudentView.xaml.cs
@@ -1,11 +1,32 @@
 using System.Windows;
+using Bai4.ViewModels;
 
 namespace Bai4.Views;
 
 public partial class StudentView : Window
 {
+    private const string JsonFileName = "data.json";
+
     public StudentView()
     {
         InitializeComponent();
+    }
+
+    private void Save_Click(object sender, RoutedEventArgs e)
+    {
+        MainViewModel? viewModel = DataContext as MainViewModel;
+        if (viewModel is not null)
+        {
+            viewModel.SaveToFile(JsonFileName);
+        }
+    }
+
+    private void Load_Click(object sender, RoutedEventArgs e)
+    {
+        MainViewModel? viewModel = DataContext as MainViewModel;
+        if (viewModel is not null)
+        {
+            viewModel.LoadFromFile(JsonFileName);
+        }
     }
 }
